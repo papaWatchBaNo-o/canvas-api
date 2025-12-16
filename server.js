5d01c0a7b12d392077b04e0ee8d6c7800e1cb7c7
Elvin Quamina <quaminaelvin@gmail.com>
2025-12-16 14:39:51 +0000

    removed the port fallback

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ connectDB();
 
 app.use("/api/forms", require("./routes/formRoutes"));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
