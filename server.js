1276bb252267114cc26962e3359900e1fda940b1
Elvin Quamina <quaminaelvin@gmail.com>
2026-02-02 09:49:05 +0000

    cors from only allowed origin

--- server.js
+++ server.js
@@ -7,7 +7,13 @@ const app = express();
 
 app.set("trust proxy", 1);
 
-app.use(cors());
+const corsOptions = {
+  origin: process.env.CLIENT_URL, 
+  credentials: true,
+  optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 connectDB();
@@ -16,5 +22,5 @@ app.use("/api/forms", require("./routes/formRoutes"));
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
-  console.log('Server running ');
+  console.log(`Server running on port ${PORT}`);
 });
--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -3,12 +3,24 @@ const { Resend } = require("resend");
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmail = async ({ subject, html }) => {
-  await resend.emails.send({
-    from: "Portfolio <onboarding@resend.dev>",
-    to: process.env.EMAIL_TO,
-    subject,
-    html,
-  });
+  try {
+    const { data, error } = await resend.emails.send({
+      from: "Portfolio <onboarding@resend.dev>",
+      to: process.env.EMAIL_TO,
+      subject,
+      html,
+    });
+
+    if (error) {
+      console.error("Email send error:", error);
+      throw new Error(error.message);
+    }
+
+    return data;
+  } catch (error) {
+    console.error("Failed to send email:", error);
+    throw error;
+  }
 };
 
 module.exports = sendEmail;
